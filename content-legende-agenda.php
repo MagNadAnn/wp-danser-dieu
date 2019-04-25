5bd781618fd0fdad610b834c5ba99544f9013f46
MagNad <margot.nadot@gmail.com>
2019-04-25 16:53:34 +0200

    correction légende et catégories agenda

--- content-legende-agenda.php
+++ content-legende-agenda.php
@@ -3,5 +3,6 @@
     <li><span class="agenda__categorie agenda__categorie_atelier"></span> Atelier</li>
     <li><span class="agenda__categorie agenda__categorie_temoignage"></span> Témoignage-conférence</li>
     <li><span class="agenda__categorie agenda__categorie_danse"></span> Danse</li>
+    <li><span class="agenda__categorie agenda__categorie_recherche"></span> Recherche-écriture</li>
     <li><span class="agenda__categorie agenda__categorie_autre"></span> Autre</li>
 </ul>
