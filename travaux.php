91b90be15cf7892dbe7112d05c382cb45d9f2114
MagNad <margot.nadot@gmail.com>
2019-05-04 16:17:11 +0200

    homogénéize travaux's template and 404's template

--- travaux.php
+++ travaux.php
@@ -4,38 +4,30 @@ Template Name: Page en travaux
 */
 ?>
 
-<!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<title><?php bloginfo( 'name' ); ?></title>
-	<meta name="viewport" content="width=device-width">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
-	<link href="https://fonts.googleapis.com/css?family=EB+Garamond" rel="stylesheet"> 
-    <link  rel="icon" href="http://www.danserdieu.com/wp-content/uploads/2019/03/favicon.ico" type="images/x-icon">
-</head>
+<?php get_header(); ?>
 
-<body class="global-settings fond">
-
-    <div class="l-cover cover">
+<main class="l-site-content site-content">
+	<section class="l-slide l-slide_with-menu">
         <div class="l-slide-content slide-content">
             <div class="l-soleil-wrap right l-soleil_cover">
                 <div class="l-soleil theme-image soleil-04"></div>
             </div>
-            <h1 class="site-title">Danser Dieu</h1>
             <?php
                 // Start the loop.
                 while ( have_posts() ) : the_post(); ?>
+                    <h1><?php the_title(); ?></h1>
                     <?php the_content(); ?>
                 <?php
                 // End the loop.
                 endwhile;
             ?>
+            <?php wp_reset_postdata(); ?>
+
             <div class="l-danseuse-wrap left l-danseuse_cover">
                 <div class="l-danseuse theme-image danseuse-01"></div>
             </div>
         </div>
-    </div>
+    </section>
+</main>
 
 <?php get_footer(); ?>
