8b83d11498ea0ea30dc9b8cdefa4ccd0e510cd14
MagNad <margot.nadot@gmail.com>
2019-05-05 14:47:35 +0200

    patch sur le menu-icon

--- sidebar.php
+++ sidebar.php
@@ -10,16 +10,29 @@
 
 <?php
 $english = get_page_by_title( 'English' );
+$espanol = get_page_by_title( 'Espanol' );
 $ressources = get_page_by_title( 'Ressources' );
-$hasIcon = $english || $ressources;
+$hasIcon = $english || $ressources || $espanol;
 ?>
 
 <ul class="l-sidebar__widgets">
 	<?php dynamic_sidebar(); ?>
-	<li>
-		<ul class="menu-icon menu">
-			<li><a class="icon icon_ressources" href="<?php echo(get_permalink($english->ID)); ?>">Ressources</a></li>
-			<li><a class="icon icon_english" href="<?php echo(get_permalink($english->ID)); ?>">English</a></li>
-		</ul>
-	</li>
+
+	<?php if ($hasIcon) : ?>
+		<li>
+			<ul class="menu-icon menu">
+				<?php if ($ressources && get_post_status($ressources->ID) === "publish") : ?>
+					<li><a class="icon icon_ressources" href="<?php echo(get_permalink($ressources->ID)); ?>">Ressources</a></li>
+				<?php endif; ?>
+				
+				<?php if ($english && get_post_status($english->ID) === "publish") : ?>
+					<li><a class="icon icon_english" href="<?php echo(get_permalink($english->ID)); ?>">English</a></li>
+				<?php endif; ?>
+
+				<?php if ($espanol && get_post_status($espanol->ID) === "publish") : ?>
+					<li><a class="icon icon_espanol" href="<?php echo(get_permalink($espanol->ID)); ?>">Español</a></li>
+				<?php endif; ?>
+			</ul>
+		</li>
+	<?php endif; ?>
 </ul>
