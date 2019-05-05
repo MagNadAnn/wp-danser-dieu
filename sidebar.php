485f36d1b502a9e2c55ccde8cf7621a9baf261d8
MagNad <margot.nadot@gmail.com>
2019-05-05 14:22:04 +0200

    ajoute un systeme de menu d'icône très basique

--- sidebar.php
+++ sidebar.php
@@ -8,6 +8,18 @@
  */
 ?>
 
+<?php
+$english = get_page_by_title( 'English' );
+$ressources = get_page_by_title( 'Ressources' );
+$hasIcon = $english || $ressources;
+?>
+
 <ul class="l-sidebar__widgets">
 	<?php dynamic_sidebar(); ?>
+	<li>
+		<ul class="menu-icon menu">
+			<li><a class="icon icon_ressources" href="<?php echo(get_permalink($english->ID)); ?>">Ressources</a></li>
+			<li><a class="icon icon_english" href="<?php echo(get_permalink($english->ID)); ?>">English</a></li>
+		</ul>
+	</li>
 </ul>
